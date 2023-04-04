179b349ff9fb9c96d849740a7232313b737b763e
InocencioC <cordeiroturorias@gmail.com>
2023-04-04 15:26:10 +0100

    Código que diz qual é a idade que terás ano que vem

---
+++ CalcularNotas/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    double nota1 = 0.0, nota2 = 0.0,nota = 0.0;
+
+    System.out.println("Insira a primeira nota: ");
+    Scanner var = new Scanner(System.in);
+
+    System.out.println("Insira a primeira nota: ");
+    Scanner var = new Scanner(System.in);
+
+    System.out.println("Insira a primeira nota: ");
+    Scanner var = new Scanner(System.in);
+
+}
---
+++ Inocêncio/IdadeAnoSeguinte.java
@@ -0,0 +1,10 @@
+class IdadeAnoSeguinte	{
+    public	static	void	main(String[]	args) {
+    int idade;
+    int	idadeNoAnoQueVem;
+    idade = 15;
+    idadeNoAnoQueVem = idade + 1;
+System.out.println(idade);
+System.out.println("idade ano que vem " + idadeNoAnoQueVem);
+}
+}
